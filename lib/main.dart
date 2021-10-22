48274a4980d07eb396421da01a2d4e0fb53426e6
Abdulmalik <abumuhab98@gmail.com>
2021-10-22 01:48:47 +0100

    login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:restaurantfinder/screens/constants.dart';
+import 'package:restaurantfinder/constants.dart';
 import 'package:restaurantfinder/screens/landing.dart';
-
+import 'package:restaurantfinder/screens/login.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,15 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: Theme.of(context).copyWith(
-        primaryColor: kPrimaryColor
-      ),
-     routes: {
-        "/landing": (context)=> LandingScreen()
-     },
-     initialRoute: "/landing"
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: Theme.of(context).copyWith(primaryColor: kPrimaryColor),
+        routes: {
+          "/landing": (context) => LandingScreen(),
+          "/login": (context) => LoginScreen()
+        },
+        initialRoute: "/login");
   }
 }
---
+++ lib/screens/login.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:restaurantfinder/constants.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).requestFocus(new FocusNode());
+        },
+        child: Container(
+          width: double.infinity,
+          color: kPrimaryColor,
+          child: SingleChildScrollView(
+            padding: EdgeInsets.zero,
+            child: SafeArea(
+              child: Padding(
+                padding:
+                EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 30),
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height-kToolbarHeight,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.arrow_back_outlined,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        "Let's sign you in.",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 35,
+                            color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Welcome back.",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 25,
+                            color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        "You've been missed",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 25,
+                            color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(
+                        height: 100,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(
+                                color: Colors.blueGrey[500]!,
+                                width: 2,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(
+                                color: Colors.blueAccent,
+                                width: 2,
+                              ),
+                            ),
+                            hintText: "email",
+                            hintStyle: TextStyle(color: Colors.grey[600])),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide(
+                              color: Colors.blueGrey[500]!,
+                              width: 2,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide(
+                              color: Colors.blueAccent,
+                              width: 2,
+                            ),
+                          ),
+                          hintText: "password",
+                          hintStyle: TextStyle(color: Colors.grey[600]),
+                        ),
+                        obscureText: true,
+                      ),
+                      Expanded(child: Container()),
+                      Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Don't have an account? ",
+                            style: TextStyle(
+                                color: Colors.blueGrey[400], fontSize: 15),
+                          ),
+                          Text(
+                            "Register",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Align(
+                        alignment: Alignment.center,
+                        child: InkWell(
+                          onTap: () {},
+                          child: Container(
+                            height: 60,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.white,
+                            ),
+                            child: Center(
+                              child: Text(
+                                "Register",
+                                style: TextStyle(
+                                    color: kPrimaryColor,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
